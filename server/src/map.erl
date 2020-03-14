9954db4f861acc57dcff80d0caa7ca582162a2c1
exocron <github@exocron.me>
2020-03-14 17:12:04 -0400

    Hardcoded example map

---
+++ server/src/map.erl
@@ -0,0 +1,30 @@
+-module(map).
+-export([generate_map/0]).
+
+-record(laser, {x, y, direction}).
+-record(start, {x, y, direction}).
+-record(goal, {x, y}).
+-record(map, {tiles, lasers, start, goal}).
+
+generate_map() ->
+    % Random maps? In my game jam?!
+    #map{tiles = [
+        <<"WWWWWWWWWWWWWWWW">>,
+        <<"W..............W">>,
+        <<"W............G.W">>,
+        <<"W..............W">>,
+        <<"W..............W">>,
+        <<"W..............W">>,
+        <<"W..............W">>,
+        <<"W..............W">>,
+        <<"W..............W">>,
+        <<"W..............W">>,
+        <<"W..............W">>,
+        <<"W..............W">>,
+        <<"W.S............W">>,
+        <<"W..............W">>,
+        <<"WWWWWWWWWWWWWWWW">>
+    ], lasers = [
+        #laser{x = 0, y = 9, direction = right}
+    ], start = #start{x = 3, y = 12, direction = right},
+    goal = #goal{x = 13, y = 2}}.
