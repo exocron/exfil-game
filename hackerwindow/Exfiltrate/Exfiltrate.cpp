4d1e77d0cda556eb352cb2207b64dc1cb7182b11
exocron <github@exocron.me>
2020-03-15 18:33:39 -0400

    Refactored C++ for terminal window

--- hackerwindow/Exfiltrate/Exfiltrate.cpp
+++ hackerwindow/Exfiltrate/Exfiltrate.cpp
@@ -1,38 +1,45 @@
-// Exfiltrate.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <Windows.h>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
-int main()
+#pragma comment(lib, "Ws2_32.lib")
+
+void init_wsa()
+{
+	WSADATA wsaData = {0};
+
+	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (result != 0) {
+		throw std::runtime_error("WSAStartup failed: " + std::to_string(result));
+	}
+}
+
+void init_console()
 {
 	HANDLE std = GetStdHandle(STD_OUTPUT_HANDLE);
 	if (std == INVALID_HANDLE_VALUE) {
-		std::cout << "Can't enable terminal colors. A lot of stuff probably won't make sense :(\n";
-		goto skip;
+		throw std::runtime_error("GetStdHandle failed: " + std::to_string(GetLastError()));
 	}
 	DWORD mode;
 	if (!GetConsoleMode(std, &mode)) {
-		std::cout << "Can't enable terminal colors. A lot of stuff probably won't make sense :(\n";
-		goto skip;
+		throw std::runtime_error("GetConsoleMode failed: " + std::to_string(GetLastError()));
 	}
 	mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING; // enable VT100 control characters
 	if (!SetConsoleMode(std, mode)) {
-		std::cout << "Can't enable terminal colors. A lot of stuff probably won't make sense :(\n";
+		throw std::runtime_error("SetConsoleMode failed: " + std::to_string(GetLastError()));
 	}
-
-skip:
-	std::cout << "Hello \x1b[31mWorld\x1b[0m!\n";
-	system("pause");
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main()
+{
+	try {
+		init_wsa();
+		init_console();
+		std::cout << "Hello \x1b[31mWorld\x1b[0m!\n";
+	} catch (const std::runtime_error& e) {
+		std::cout << e.what() << std::endl;
+		std::cout << "Can't continue\n";
+	}
+	system("pause");
+}
