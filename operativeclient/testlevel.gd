876fe2cbd1ef77e225f0c725b8f61c9a4d9c9337
exocron <github@exocron.me>
2020-03-22 13:13:47 -0400

    Added movable green square as stand-in for player

--- operativeclient/testlevel.gd
+++ operativeclient/testlevel.gd
@@ -6,3 +6,20 @@ func _ready():
 func _on_game_object_changed(object):
 	if object["type"] == "laser" and object["name"] == "las0":
 		$las0.visible = object["enabled"]
+		$las0/Area2D/CollisionShape2D.disabled = !object["enabled"]
+
+func _process(delta):
+	var direction = Vector2(0, 0)
+	if Input.is_action_pressed("ui_up"):
+		direction += Vector2(0, -1)
+	if Input.is_action_pressed("ui_down"):
+		direction += Vector2(0, 1)
+	if Input.is_action_pressed("ui_left"):
+		direction += Vector2(-1, 0)
+	if Input.is_action_pressed("ui_right"):
+		direction += Vector2(1, 0)
+	$player.translate(direction.normalized() * 2)
+
+func _on_Area2D_area_entered(area):
+	if area.get_parent().is_in_group("hazard"):
+		$player.visible = false
